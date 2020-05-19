5a0d9efba93561bb27f7564a0425218b3e61f61f
Jessica <github email address>
2020-05-18 21:53:31 -0400

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,23 @@
+class Appointment < ApplicationRecord
+    belongs_to :doctor
+    belongs_to :patient
+
+    def doctor_name=(name)
+        self.doctor = Doctor.find_or_create_by(name: name)
+    end
+
+    def doctor_name
+        doctor.try(:name)
+        #self.doctor ? self.doctor.name : nil
+    end
+
+    def patient_name=(name)
+        self.patient = Patient.find_or_create_by(name: name)
+    end
+
+    def patient_name
+        patient.try(:name)
+        #self.patient ? self.patient.name : nil
+    end
+
+end
